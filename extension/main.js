dbb7dc114d55df263e44cc3bdc4e671054e952aa
SarahNathanson <sarah.j.nathanson@gmail.com>
2018-01-27 15:50:07 -0500

    date function

---
+++ extension/main.js
@@ -0,0 +1,10 @@
+document.getElementById("date").innerHTML=
+document.getElementById("time").innerHTML=
+
+function getDate(){
+	var date = new Date("10/11/2009"),
+    locale = "en-us",
+    month = date.toLocaleString(locale, { month: "long" });
+
+    
+}
