2549406b5dacffa425a8fae1080a3a1dd9ff0d9c
SarahNathanson <sarah.j.nathanson@gmail.com>
2018-01-28 01:50:19 -0500

    fixed format

--- extension/main.js
+++ extension/main.js
@@ -81,18 +81,18 @@ $.ajax({
 });
 });
 
-//$.ajax({
-//	url: "http://localhost:8000/weather"
-//	type: "get",
-//	data: {
-//		latitude: getLocation().coords.latitude
-//		longtitude: getLocation().coords.longitude
-//	}
-//})
+$.ajax({
+	url: "http://localhost:8000/weather",
+	type: "get",
+	data: {
+		latitude: getLocation().coords.latitude,
+		longtitude: getLocation().coords.longitude
+	}
+});
 
 function getWeather() {
 	var weatherFormat = document.getElementById("weather");
-	var loc = getLocation()
+	var loc = getLocation();
 }
 
 function getLocation() {
